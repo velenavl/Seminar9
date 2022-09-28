78d41d1324809c4884f108351a5c51d2a6e33df3
Elena <velenavl@inbox.ru>
2022-09-29 00:28:42 +0300

    HW064

---
+++ HW064/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string NaturalNumber(int M, int N)
+{
+    if (M < N)
+        return $"{M} " + NaturalNumber(M + 1, N);
+    else
+        return $"{M}";
+}
+
+Console.WriteLine(NaturalNumber(1, 5));
