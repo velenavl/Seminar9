90edcc1906b9932c630462575d103bca0295a9d0
Elena <velenavl@inbox.ru>
2022-09-29 23:56:18 +0300

    HW066

---
+++ HW066/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+
+int SumRec(int n)
+{
+    if (n == 4) return 4;
+    else return n + SumRec(n - 1);
+}
+
+// Console.WriteLine(SumRec(15)); 
+Console.WriteLine(SumRec(8)); 
